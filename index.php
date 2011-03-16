69d2bad4f9d55aa9dc9e61e42d1ad0d24d679389
Daniel Houston <danielhouston@monsterzcave.co.uk>
2011-03-16 17:30:15 +0000

    Styling changes to Background and text
    Background now uses CSS3 Gradients and text is now using CSS @FontFace, Delicious font.

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
 		<link href="stylingCSS.css" rel="stylesheet" type="text/css"> 
 	</head>
 	<body>
+		<div>
 	
-		<?php 
-		include "header.php";
-		?>
-
-		<?php 
-		include "footer.php";
-		?>
-
+			<?php 
+			include "header.php";
+			?>
+	
+			<?php 
+			include "footer.php";
+			?>
+		</div>	
 	</body>
 </html>
